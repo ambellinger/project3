2173de10735807188da425ad5e8ee483835c9d25
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2019-03-20 17:20:07 -0500

    adding working server stuff

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav";
 import Jumbotron from "../components/Jumbotron"
+// import API from "../utils/API";
 
 
 
 class Main extends Component{
 
+//     state ={
+//         buildings: []
+//     }
 
+// componentDidMount(){
+//     this.loadBuildings()
+// }
 render(){
     return(
 <div>
---
+++ old_client/build/precache-manifest.42375145261a7e83fd24c2ad309c8062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a9d6399db6f5a86ffe1",
+    "url": "/static/js/main.dbc6cda5.chunk.js"
+  },
+  {
+    "revision": "5bfb9215399605a56874",
+    "url": "/static/js/2.2ae24be5.chunk.js"
+  },
+  {
+    "revision": "1a9d6399db6f5a86ffe1",
+    "url": "/static/css/main.3e7a034c.chunk.css"
+  },
+  {
+    "revision": "106ea2a051afb78f35c592b49c56fedd",
+    "url": "/index.html"
+  }
+];
